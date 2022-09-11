d46ced8ae761251f1e41e83483fd10b285afdb2c
mihricankizilyer <mihricankizilyer@hotmail.com>
2022-09-11 17:19:53 +0300

    Create 04_alphabet_rangoli.py

---
+++ 04_alphabet_rangoli.py
@@ -0,0 +1,12 @@
+import string
+alpha = string.ascii_lowercase # getting english alphabet
+
+def print_rangoli(size):
+    M = []
+    for i in range(0,size):
+        letters = "-".join(alpha[i:size]) # base letters with "-" combine
+        M.append((letters[::-1] + letters[1:] ).center(4*size-3, "-")) # all type line inside list
+
+    print("\n".join(M[::-1] + M[1:]))  # up and down get together
+
+print_rangoli(15)
